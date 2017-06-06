0a944dbdaf697f4dea15264faf6da12335d42697
Jake Champlin <jake.champlin.27@gmail.com>
2017-06-06 12:49:08 -0400

    Initial transfer of provider code

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import (
+	"github.com/hashicorp/terraform/plugin"
+	"github.com/terraform-providers/terraform-provider-vsphere/vsphere"
+)
+
 func main() {
-	/*plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: opc.Provider})*/
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: vsphere.Provider})
 }
