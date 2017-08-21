43972cc76bae9f5b01216c8f85bca3c448b67645
Chris Marchesi <chrism@vancluevertech.com>
2017-08-21 16:15:24 -0700

    r/host_port_group: More tests
    For complex configs, including overrides where we change NIC order and a
    security attribute.
    
    This should be good to go now - I will start writing docs :)

--- vsphere/resource_vsphere_host_port_group_test.go
+++ vsphere/resource_vsphere_host_port_group_test.go
@@ -1,15 +1,16 @@
 package vsphere
 
 import (
-	"errors"
 	"fmt"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
 	"github.com/vmware/govmomi"
+	"github.com/vmware/govmomi/vim25/types"
 )
 
 func TestAccResourceVSphereHostPortGroup(t *testing.T) {
@@ -37,6 +38,58 @@ func TestAccResourceVSphereHostPortGroup(t *testing.T) {
 				},
 			},
 		},
+		{
+			"more complex configuration and overridden attributes",
+			resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(tp)
+					testAccResourceVSphereHostPortGroupPreCheck(tp)
+				},
+				Providers:    testAccProviders,
+				CheckDestroy: testAccResourceVSphereHostPortGroupExists(false),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccResourceVSphereHostPortGroupConfigWithOverrides(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccResourceVSphereHostPortGroupExists(true),
+							testAccResourceVSphereHostPortGroupCheckVlan(1000),
+							testAccResourceVSphereHostPortGroupCheckEffectiveActive([]string{os.Getenv("VSPHERE_HOST_NIC0")}),
+							testAccResourceVSphereHostPortGroupCheckEffectiveStandby([]string{os.Getenv("VSPHERE_HOST_NIC1")}),
+							testAccResourceVSphereHostPortGroupCheckEffectivePromisc(true),
+						),
+					},
+				},
+			},
+		},
+		{
+			"basic, then complex config",
+			resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(tp)
+					testAccResourceVSphereHostPortGroupPreCheck(tp)
+				},
+				Providers:    testAccProviders,
+				CheckDestroy: testAccResourceVSphereHostPortGroupExists(false),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccResourceVSphereHostPortGroupConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccResourceVSphereHostPortGroupExists(true),
+						),
+					},
+					{
+						Config: testAccResourceVSphereHostPortGroupConfigWithOverrides(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccResourceVSphereHostPortGroupExists(true),
+							testAccResourceVSphereHostPortGroupCheckVlan(1000),
+							testAccResourceVSphereHostPortGroupCheckEffectiveActive([]string{os.Getenv("VSPHERE_HOST_NIC0")}),
+							testAccResourceVSphereHostPortGroupCheckEffectiveStandby([]string{os.Getenv("VSPHERE_HOST_NIC1")}),
+							testAccResourceVSphereHostPortGroupCheckEffectivePromisc(true),
+						),
+					},
+				},
+			},
+		},
 	}
 
 	for _, tc := range testAccResourceVSphereHostPortGroupCases {
@@ -59,28 +112,104 @@ func testAccResourceVSphereHostPortGroupPreCheck(t *testing.T) {
 	}
 }
 
+// testGetPortGroup is a convenience method to fetch a static port group
+// resource for testing.
+func testGetPortGroup(s *terraform.State, name, host, datacenter string) (*types.HostPortGroup, error) {
+	rs, ok := s.RootModule().Resources[fmt.Sprintf("vsphere_host_port_group.%s", name)]
+	if !ok {
+		return nil, fmt.Errorf("vsphere_host_port_group.%s not found in state", name)
+	}
+
+	client := testAccProvider.Meta().(*govmomi.Client)
+	id := rs.Primary.ID
+	timeout := time.Minute * 5
+	return hostPortGroupFromName(client, id, host, datacenter, timeout)
+}
+
 func testAccResourceVSphereHostPortGroupExists(expected bool) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		rs, ok := s.RootModule().Resources["vsphere_host_port_group.pg"]
-		if !ok {
-			return errors.New("vsphere_host_port_group.pg not found in state")
-		}
-
-		client := testAccProvider.Meta().(*govmomi.Client)
-		id := rs.Primary.ID
+		name := "PGTerraformTest"
+		id := "pg"
 		host := os.Getenv("VSPHERE_ESXI_HOST")
 		datacenter := os.Getenv("VSPHERE_DATACENTER")
-		timeout := time.Minute * 5
-		_, err := hostPortGroupFromName(client, id, host, datacenter, timeout)
+		_, err := testGetPortGroup(s, id, host, datacenter)
 		if err != nil {
-			if err.Error() == fmt.Sprintf("port group %s not found on host %s", id, host) && !expected {
+			if err.Error() == fmt.Sprintf("port group %s not found on host %s", name, host) && !expected {
 				// Expected missing
 				return nil
 			}
 			return err
 		}
 		if !expected {
-			return fmt.Errorf("expected port group %s to still be missing", id)
+			return fmt.Errorf("expected port group %s to still be missing", name)
+		}
+		return nil
+	}
+}
+
+func testAccResourceVSphereHostPortGroupCheckVlan(expected int32) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		id := "pg"
+		host := os.Getenv("VSPHERE_ESXI_HOST")
+		datacenter := os.Getenv("VSPHERE_DATACENTER")
+		pg, err := testGetPortGroup(s, id, host, datacenter)
+		if err != nil {
+			return err
+		}
+		actual := pg.Spec.VlanId
+		if expected != actual {
+			return fmt.Errorf("expected VLAN ID to be %d, got %d", expected, actual)
+		}
+		return nil
+	}
+}
+
+func testAccResourceVSphereHostPortGroupCheckEffectiveActive(expected []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		id := "pg"
+		host := os.Getenv("VSPHERE_ESXI_HOST")
+		datacenter := os.Getenv("VSPHERE_DATACENTER")
+		pg, err := testGetPortGroup(s, id, host, datacenter)
+		if err != nil {
+			return err
+		}
+		actual := pg.ComputedPolicy.NicTeaming.NicOrder.ActiveNic
+		if !reflect.DeepEqual(expected, actual) {
+			return fmt.Errorf("expected effective active NICs to be %#v, got %#v", expected, actual)
+		}
+		return nil
+	}
+}
+
+func testAccResourceVSphereHostPortGroupCheckEffectiveStandby(expected []string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		name := "pg"
+		host := os.Getenv("VSPHERE_ESXI_HOST")
+		datacenter := os.Getenv("VSPHERE_DATACENTER")
+		pg, err := testGetPortGroup(s, name, host, datacenter)
+		if err != nil {
+			return err
+		}
+		actual := pg.ComputedPolicy.NicTeaming.NicOrder.StandbyNic
+		if !reflect.DeepEqual(expected, actual) {
+			return fmt.Errorf("expected effective standby NICs to be %#v, got %#v", expected, actual)
+		}
+		return nil
+	}
+}
+
+func testAccResourceVSphereHostPortGroupCheckEffectivePromisc(expected bool) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		name := "pg"
+		host := os.Getenv("VSPHERE_ESXI_HOST")
+		datacenter := os.Getenv("VSPHERE_DATACENTER")
+		pg, err := testGetPortGroup(s, name, host, datacenter)
+		if err != nil {
+			return err
+		}
+		actual := *pg.ComputedPolicy.Security.AllowPromiscuous
+		if expected != actual {
+			return fmt.Errorf("expected effective allow promiscuous to be %t, got %t", expected, actual)
 		}
 		return nil
 	}
@@ -123,6 +252,56 @@ resource "vsphere_host_port_group" "pg" {
   host                = "${var.esxi_host}"
   datacenter          = "${var.datacenter}"
   virtual_switch_name = "${vsphere_host_virtual_switch.switch.id}"
+}
+`, os.Getenv("VSPHERE_ESXI_HOST"), os.Getenv("VSPHERE_DATACENTER"), os.Getenv("VSPHERE_HOST_NIC0"), os.Getenv("VSPHERE_HOST_NIC1"))
+}
+
+func testAccResourceVSphereHostPortGroupConfigWithOverrides() string {
+	return fmt.Sprintf(`
+variable "esxi_host" {
+  type    = "string"
+  default = "%s"
+}
+
+variable "datacenter" {
+  type    = "string"
+  default = "%s"
+}
+
+variable "host_nic0" {
+  type    = "string"
+  default = "%s"
+}
+
+variable "host_nic1" {
+  type    = "string"
+  default = "%s"
+}
+
+resource "vsphere_host_virtual_switch" "switch" {
+  name             = "vSwitchTerraformTest"
+  host             = "${var.esxi_host}"
+  datacenter       = "${var.datacenter}"
+  network_adapters = ["${var.host_nic0}", "${var.host_nic1}"]
+
+  active_nics  = ["${var.host_nic0}", "${var.host_nic1}"]
+  standby_nics = []
+
+  allow_promiscuous = false
+}
+
+resource "vsphere_host_port_group" "pg" {
+  name                = "PGTerraformTest"
+  host                = "${var.esxi_host}"
+  datacenter          = "${var.datacenter}"
+  virtual_switch_name = "${vsphere_host_virtual_switch.switch.id}"
+
+  vlan_id = 1000
+
+  active_nics  = ["${var.host_nic0}"]
+  standby_nics = ["${var.host_nic1}"]
+
+  allow_promiscuous = true
 }
 `, os.Getenv("VSPHERE_ESXI_HOST"), os.Getenv("VSPHERE_DATACENTER"), os.Getenv("VSPHERE_HOST_NIC0"), os.Getenv("VSPHERE_HOST_NIC1"))
 }
