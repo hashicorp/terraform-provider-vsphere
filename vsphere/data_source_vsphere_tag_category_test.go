6e411eefecb97d969622d340fe7d12ecf32ef81b
Chris Marchesi <chrism@vancluevertech.com>
2017-09-19 14:35:28 -0700

    New data source: vsphere_tag_category
    This is the data source counterpart to the new vsphere_tag_category
    resource. It allows one to pull in an external tag category by name,
    using most of the same search call stack as importing.

---
+++ vsphere/data_source_vsphere_tag_category_test.go
@@ -0,0 +1,115 @@
+package vsphere
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceVSphereTagCategory(t *testing.T) {
+	var tp *testing.T
+	testAccDataSourceVSphereTagCategoryCases := []struct {
+		name     string
+		testCase resource.TestCase
+	}{
+		{
+			"basic",
+			resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(tp)
+				},
+				Providers: testAccProviders,
+				Steps: []resource.TestStep{
+					{
+						Config: testAccDataSourceVSphereTagCategoryConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							resource.TestCheckResourceAttr(
+								"data.vsphere_tag_category.terraform-test-category-data",
+								"name",
+								testAccDataSourceVSphereTagCategoryConfigName,
+							),
+							resource.TestCheckResourceAttr(
+								"data.vsphere_tag_category.terraform-test-category-data",
+								"description",
+								testAccDataSourceVSphereTagCategoryConfigDescription,
+							),
+							resource.TestCheckResourceAttr(
+								"data.vsphere_tag_category.terraform-test-category-data",
+								"cardinality",
+								testAccDataSourceVSphereTagCategoryConfigCardinality,
+							),
+							resource.TestCheckResourceAttr(
+								"data.vsphere_tag_category.terraform-test-category-data",
+								"associable_types.#",
+								"1",
+							),
+							resource.TestCheckResourceAttr(
+								"data.vsphere_tag_category.terraform-test-category-data",
+								"associable_types.3125094965",
+								testAccDataSourceVSphereTagCategoryConfigAssociableType,
+							),
+							resource.TestCheckResourceAttrPair(
+								"data.vsphere_tag_category.terraform-test-category-data", "id",
+								"vsphere_tag_category.terraform-test-category", "id",
+							),
+						),
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testAccDataSourceVSphereTagCategoryCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tp = t
+			resource.Test(t, tc.testCase)
+		})
+	}
+}
+
+const testAccDataSourceVSphereTagCategoryConfigName = "terraform-test-category"
+const testAccDataSourceVSphereTagCategoryConfigDescription = "Managed by Terraform"
+const testAccDataSourceVSphereTagCategoryConfigCardinality = vSphereTagCategoryCardinalitySingle
+const testAccDataSourceVSphereTagCategoryConfigAssociableType = vSphereTagCategoryAssociableTypeVirtualMachine
+
+func testAccDataSourceVSphereTagCategoryConfig() string {
+	return fmt.Sprintf(`
+variable "tag_category_name" {
+  default = "%s"
+}
+
+variable "tag_category_description" {
+  default = "%s"
+}
+
+variable "tag_category_cardinality" {
+  default = "%s"
+}
+
+variable "tag_category_associable_types" {
+  default = [
+    "%s",
+  ]
+}
+
+resource "vsphere_tag_category" "terraform-test-category" {
+  name        = "${var.tag_category_name}"
+  description = "${var.tag_category_description}"
+  cardinality = "${var.tag_category_cardinality}"
+
+  associable_types = [
+    "${var.tag_category_associable_types}",
+  ]
+}
+
+data "vsphere_tag_category" "terraform-test-category-data" {
+  name = "${vsphere_tag_category.terraform-test-category.name}"
+}
+`,
+		testAccDataSourceVSphereTagCategoryConfigName,
+		testAccDataSourceVSphereTagCategoryConfigDescription,
+		testAccDataSourceVSphereTagCategoryConfigCardinality,
+		testAccDataSourceVSphereTagCategoryConfigAssociableType,
+	)
+}
--- vsphere/tags_helper.go
+++ vsphere/tags_helper.go
@@ -1,8 +1,25 @@
 package vsphere
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/vmware/govmomi"
+	"github.com/vmware/vic/pkg/vsphere/tags"
 )
+
+// vSphereTagCategorySearchErrMultiple is an error message format for a tag
+// category search that returned multiple results. This is a bug and needs to
+// be reported so we can adjust the API.
+const vSphereTagCategorySearchErrMultiple = `
+Category name %q returned multiple results!
+
+This is a bug - please report it at:
+https://github.com/terraform-providers/terraform-provider-vsphere/issues
+
+This version of the provider requires unique category names. To work around
+this issue, please use a category name unique within your vCenter system.
+`
 
 // tagsMinVersion is the minimum vSphere version required for tags.
 var tagsMinVersion = vSphereVersion{
@@ -23,4 +40,29 @@ func isEligibleTagEndpoint(client *govmomi.Client) bool {
 		return false
 	}
 	return true
+}
+
+// tagCategoryByName locates a tag category by name. It's used by the
+// vsphere_tag_category data source, and the resource importer.
+func tagCategoryByName(client *tags.RestClient, name string) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), defaultAPITimeout)
+	defer cancel()
+	cats, err := client.GetCategoriesByName(ctx, name)
+	if err != nil {
+		return "", fmt.Errorf("could not get category for name %q: %s", name, err)
+	}
+
+	if len(cats) < 1 {
+		return "", fmt.Errorf("category name %q not found", name)
+	}
+	if len(cats) > 1 {
+		// Although GetCategoriesByName does not seem to think that tag categories
+		// are unique, empirical observation via the console and API show that they
+		// are. If for some reason the returned results includes more than one ID,
+		// we give an error, indicating that this is a bug and the user should
+		// submit an issue.
+		return "", fmt.Errorf(vSphereTagCategorySearchErrMultiple, name)
+	}
+
+	return cats[0].ID, nil
 }
