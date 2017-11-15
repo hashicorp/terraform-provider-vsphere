82eec3172b5befa5aa365d9769dd654d01ac5623
Chris Marchesi <chrism@vancluevertech.com>
2017-11-14 19:05:46 -0800

    r/virtual_machine: State migration for state version 2
    This is treated nearly like an import - we just grab the UUID, ensure it
    exists, validate that the disks will work with the new version of the
    resource, and then change the ID. The VM is also flagged as imported so
    that cloning can't be added.

--- vsphere/resource_vsphere_virtual_machine_migrate.go
+++ vsphere/resource_vsphere_virtual_machine_migrate.go
@@ -6,32 +6,100 @@ import (
 	"strings"
 
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-vsphere/vsphere/internal/helper/virtualmachine"
+	"github.com/terraform-providers/terraform-provider-vsphere/vsphere/internal/virtualdevice"
+	"github.com/vmware/govmomi/object"
 )
 
-func resourceVSphereVirtualMachineMigrateState(
-	v int, is *terraform.InstanceState, meta interface{}) (*terraform.InstanceState, error) {
-	if is.Empty() {
-		log.Println("[DEBUG] Empty InstanceState; nothing to migrate.")
-		return is, nil
+// resourceVSphereVirtualMachineMigrateState is the master state migration function for
+// the vsphere_virtual_machine resource.
+func resourceVSphereVirtualMachineMigrateState(version int, os *terraform.InstanceState, meta interface{}) (*terraform.InstanceState, error) {
+	// Guard against a nil state.
+	if os == nil {
+		return nil, nil
 	}
 
-	switch v {
-	case 0:
-		log.Println("[INFO] Found Compute Instance State v0; migrating to v1")
-		is, err := migrateVSphereVirtualMachineStateV0toV1(is)
-		if err != nil {
-			return is, err
-		}
-		return is, nil
-	default:
-		return is, fmt.Errorf("Unexpected schema version: %d", v)
+	// Guard against empty state, can't do anything with it
+	if os.Empty() {
+		return os, nil
 	}
+
+	var migrateFunc func(*terraform.InstanceState, interface{}) error
+	switch version {
+	case 1:
+		log.Printf("[DEBUG] Migrating vsphere_virtual_machine state: old v%d state: %#v", version, os)
+		migrateFunc = migrateVSphereVirtualMachineStateV2
+	case 0:
+		log.Printf("[DEBUG] Migrating vsphere_virtual_machine state: old v%d state: %#v", version, os)
+		migrateFunc = migrateVSphereVirtualMachineStateV1
+	default:
+		// Migration is complete
+		log.Printf("[DEBUG] Migrating vsphere_virtual_machine state: completed v%d state: %#v", version, os)
+		return os, nil
+	}
+	if err := migrateFunc(os, meta); err != nil {
+		return nil, err
+	}
+	version++
+	log.Printf("[DEBUG] Migrating vsphere_virtual_machine state: new v%d state: %#v", version, os)
+	return resourceVSphereVirtualMachineMigrateState(version, os, meta)
 }
 
-func migrateVSphereVirtualMachineStateV0toV1(is *terraform.InstanceState) (*terraform.InstanceState, error) {
+// migrateVSphereVirtualMachineStateV2 migrates the state of the
+// vsphere_virtual_machine from version 1 to version 2.
+func migrateVSphereVirtualMachineStateV2(is *terraform.InstanceState, meta interface{}) error {
+	// All we really preserve from the old state is the UUID of the virtual
+	// machine. We leverage some of the special parts of the import functionality
+	// - namely validating disks, and flagging the VM as imported in the state to
+	// guard against someone adding customization to the configuration and
+	// accidentally forcing a new resource.
+	//
+	// Read will handle most of the population post-migration as it does for
+	// import, and there will be an unavoidable diff for TF-only options on the
+	// next plan.
+	client := meta.(*VSphereClient).vimClient
+	name := is.ID
+	id := is.Attributes["uuid"]
+	if id == "" {
+		return fmt.Errorf("resource ID %s has no UUID. State cannot be migrated", name)
+	}
+
+	log.Printf("[DEBUG] Migrate state for VM resource %q: UUID %q", name, id)
+	vm, err := virtualmachine.FromUUID(client, id)
+	if err != nil {
+		return fmt.Errorf("error fetching virtual machine: %s", err)
+	}
+	props, err := virtualmachine.Properties(vm)
+	if err != nil {
+		return fmt.Errorf("error fetching virtual machine properties: %s", err)
+	}
+
+	// Validate the disks in the VM to make sure that they will work with the new
+	// version of the resource. This is mainly ensuring that all disks are SCSI
+	// disks, but a Read operation is attempted as well to make sure it will
+	// survive that.
+	//
+	// NOTE: This uses the current version of the resource to make this check,
+	// which at some point in time may end up being a higher schema version than
+	// version 2. At this point in time, there is nothing here that would cause
+	// issues (nothing in the sub-resource read logic is reliant on schema
+	// versions), and an empty ResourceData is sent anyway.
+	d := resourceVSphereVirtualMachine().Data(&terraform.InstanceState{})
+	if err := virtualdevice.DiskImportOperation(d, client, object.VirtualDeviceList(props.Config.Hardware.Device)); err != nil {
+		return err
+	}
+	// The VM should be ready for reading now
+	is.Attributes = make(map[string]string)
+	is.ID = id
+	is.Attributes["imported"] = "true"
+	log.Printf("[DEBUG] %s: Migration complete, resource is ready for read", resourceVSphereVirtualMachineIDString(d))
+	return nil
+}
+
+func migrateVSphereVirtualMachineStateV1(is *terraform.InstanceState, meta interface{}) error {
 	if is.Empty() || is.Attributes == nil {
 		log.Println("[DEBUG] Empty VSphere Virtual Machine State; nothing to migrate.")
-		return is, nil
+		return nil
 	}
 
 	log.Printf("[DEBUG] Attributes before migration: %#v", is.Attributes)
@@ -44,7 +112,7 @@ func migrateVSphereVirtualMachineStateV0toV1(is *terraform.InstanceState) (*terr
 		is.Attributes["enable_disk_uuid"] = "false"
 	}
 
-	for k, _ := range is.Attributes {
+	for k := range is.Attributes {
 		if strings.HasPrefix(k, "disk.") && strings.HasSuffix(k, ".size") {
 			diskParts := strings.Split(k, ".")
 			if len(diskParts) != 3 {
@@ -58,5 +126,5 @@ func migrateVSphereVirtualMachineStateV0toV1(is *terraform.InstanceState) (*terr
 	}
 
 	log.Printf("[DEBUG] Attributes after migration: %#v", is.Attributes)
-	return is, nil
+	return nil
 }
--- vsphere/resource_vsphere_virtual_machine_migrate_test.go
+++ vsphere/resource_vsphere_virtual_machine_migrate_test.go
@@ -1,34 +1,42 @@
 package vsphere
 
 import (
+	"os"
+	"path"
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/terraform-providers/terraform-provider-vsphere/vsphere/internal/helper/virtualmachine"
 )
 
-func TestVSphereVirtualMachineMigrateState(t *testing.T) {
+func testAccResourceVSphereVirtualMachineMigrateStatePreCheck(t *testing.T) {
+	if os.Getenv("TF_ACC") == "" {
+		t.Skip("set TF_ACC to run vsphere_virtual_machine state migration tests (provider connection is required)")
+	}
+	if os.Getenv("VSPHERE_VM_V1_PATH") == "" {
+		t.Skip("set VSPHERE_VM_V1_PATH to run vsphere_virtual_machine state migration tests")
+	}
+}
+
+func TestVSphereVirtualMachineMigrateStateV1(t *testing.T) {
 	cases := map[string]struct {
-		StateVersion int
-		Attributes   map[string]string
-		Expected     map[string]string
-		Meta         interface{}
+		Attributes map[string]string
+		Expected   map[string]string
 	}{
 		"skip_customization before 0.6.16": {
-			StateVersion: 0,
-			Attributes:   map[string]string{},
+			Attributes: map[string]string{},
 			Expected: map[string]string{
 				"skip_customization": "false",
 			},
 		},
 		"enable_disk_uuid before 0.6.16": {
-			StateVersion: 0,
-			Attributes:   map[string]string{},
+			Attributes: map[string]string{},
 			Expected: map[string]string{
 				"enable_disk_uuid": "false",
 			},
 		},
 		"disk controller_type": {
-			StateVersion: 0,
 			Attributes: map[string]string{
 				"disk.1234.size":            "0",
 				"disk.5678.size":            "0",
@@ -51,10 +59,7 @@ func TestVSphereVirtualMachineMigrateState(t *testing.T) {
 			ID:         "i-abc123",
 			Attributes: tc.Attributes,
 		}
-		is, err := resourceVSphereVirtualMachineMigrateState(
-			tc.StateVersion, is, tc.Meta)
-
-		if err != nil {
+		if err := migrateVSphereVirtualMachineStateV1(is, nil); err != nil {
 			t.Fatalf("bad: %s, err: %#v", tn, err)
 		}
 
@@ -65,6 +70,47 @@ func TestVSphereVirtualMachineMigrateState(t *testing.T) {
 					tn, k, v, k, is.Attributes[k], is.Attributes)
 			}
 		}
+	}
+}
+
+func TestAccResourceVSphereVirtualMachineMigrateStateV2(t *testing.T) {
+	testAccResourceVSphereVirtualMachineMigrateStatePreCheck(t)
+	testAccPreCheck(t)
+
+	meta, err := testAccProviderMeta(t)
+	if err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+
+	client := meta.(*VSphereClient).vimClient
+	pth := os.Getenv("VSPHERE_VM_V1_PATH")
+	name := path.Base(pth)
+	vm, err := virtualmachine.FromPath(client, pth, nil)
+	if err != nil {
+		t.Fatalf("error fetching virtual machine: %s", err)
+	}
+	props, err := virtualmachine.Properties(vm)
+	if err != nil {
+		t.Fatalf("error fetching virtual machine properties: %s", err)
+	}
+
+	is := &terraform.InstanceState{
+		ID: name,
+		Attributes: map[string]string{
+			"uuid": props.Config.Uuid,
+		},
+	}
+	// Start this at version 0 so we know it go through the whole path. There's
+	// currently nothing from v0 to v2 that should hinder this.
+	is, err = resourceVSphereVirtualMachineMigrateState(0, is, meta)
+	if err != nil {
+		t.Fatalf("bad: %s", err)
+	}
+	if matched, _ := regexp.Match("^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$", []byte(is.ID)); !matched {
+		t.Fatalf("expected ID to be a UUID, got ID as %q", is.ID)
+	}
+	if is.Attributes["imported"] != "true" {
+		t.Fatalf("expected imported to be true")
 	}
 }
 
