b1693710a236d3d13cd95b0aaeb626ea230f3192
Chris Marchesi <chrism@vancluevertech.com>
2018-03-01 10:54:24 -0800

    d/custom_attribute: Convert acceptance tests to non-subtests

--- vsphere/data_source_vsphere_custom_attribute_test.go
+++ vsphere/data_source_vsphere_custom_attribute_test.go
@@ -7,50 +7,34 @@ import (
 	"github.com/hashicorp/terraform/helper/resource"
 )
 
-func TestAccDataSourceVSphereCustomAttribute(t *testing.T) {
-	var tp *testing.T
-	testAccDataSourceVSphereCustomAttributeCases := []struct {
-		name     string
-		testCase resource.TestCase
-	}{
-		{
-			"basic",
-			resource.TestCase{
-				PreCheck: func() {
-					testAccPreCheck(tp)
-				},
-				Providers: testAccProviders,
-				Steps: []resource.TestStep{
-					{
-						Config: testAccDataSourceVSphereCustomAttributeConfig(),
-						Check: resource.ComposeTestCheckFunc(
-							resource.TestCheckResourceAttr(
-								"data.vsphere_custom_attribute.terraform-test-attribute-data",
-								"name",
-								testAccDataSourceVSphereCustomAttributeConfigName,
-							),
-							resource.TestCheckResourceAttr(
-								"data.vsphere_custom_attribute.terraform-test-attribute-data",
-								"managed_object_type",
-								testAccDataSourceVSphereCustomAttributeConfigType,
-							),
-							resource.TestCheckResourceAttrPair(
-								"data.vsphere_custom_attribute.terraform-test-attribute-data", "id",
-								"vsphere_custom_attribute.terraform-test-attribute", "id",
-							),
-						),
-					},
-				},
+func TestAccDataSourceVSphereCustomAttribute_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceVSphereCustomAttributeConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.vsphere_custom_attribute.terraform-test-attribute-data",
+						"name",
+						testAccDataSourceVSphereCustomAttributeConfigName,
+					),
+					resource.TestCheckResourceAttr(
+						"data.vsphere_custom_attribute.terraform-test-attribute-data",
+						"managed_object_type",
+						testAccDataSourceVSphereCustomAttributeConfigType,
+					),
+					resource.TestCheckResourceAttrPair(
+						"data.vsphere_custom_attribute.terraform-test-attribute-data", "id",
+						"vsphere_custom_attribute.terraform-test-attribute", "id",
+					),
+				),
 			},
 		},
-	}
-
-	for _, tc := range testAccDataSourceVSphereCustomAttributeCases {
-		t.Run(tc.name, func(t *testing.T) {
-			tp = t
-			resource.Test(t, tc.testCase)
-		})
-	}
+	})
 }
 
 const testAccDataSourceVSphereCustomAttributeConfigName = "terraform-test-attribute"
