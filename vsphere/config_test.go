4030330b9dbeaf210fe0b12625c98bb040aafd29
Chris Marchesi <chrism@vancluevertech.com>
2018-03-06 19:49:21 -0800

    provider: Add acceptance tests for session persistence
    One test to test for persistence, and another to test to ensure that
    sessions did not persist.

---
+++ vsphere/config_test.go
@@ -0,0 +1,143 @@
+package vsphere
+
+import (
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func testAccClientGenerateConfig(t *testing.T) *Config {
+	testAccPreCheck(t)
+	insecure, _ := strconv.ParseBool(os.Getenv("VSPHERE_ALLOW_UNVERIFIED_SSL"))
+	debug, _ := strconv.ParseBool(os.Getenv("VSPHERE_CLIENT_DEBUG"))
+
+	return &Config{
+		InsecureFlag:  insecure,
+		Debug:         debug,
+		User:          os.Getenv("VSPHERE_USER"),
+		Password:      os.Getenv("VSPHERE_PASSWORD"),
+		VSphereServer: os.Getenv("VSPHERE_SERVER"),
+		DebugPath:     os.Getenv("VSPHERE_CLIENT_DEBUG_PATH"),
+		DebugPathRun:  os.Getenv("VSPHERE_CLIENT_DEBUG_PATH_RUN"),
+	}
+}
+
+func testAccClientGenerateData(t *testing.T, c *Config) (string, string) {
+	_, err := c.Client()
+	if err != nil {
+		t.Fatalf("error setting up client: %s", err)
+	}
+
+	vimSessionFile, err := c.vimSessionFile()
+	if err != nil {
+		t.Fatalf("error computing VIM session file: %s", err)
+	}
+	restSessionFile, err := c.restSessionFile()
+	if err != nil {
+		t.Fatalf("error computing REST session file: %s", err)
+	}
+
+	vimData, err := ioutil.ReadFile(vimSessionFile)
+	if err != nil {
+		t.Fatalf("error reading VIM session file: %s", err)
+	}
+
+	restData, err := ioutil.ReadFile(restSessionFile)
+	if err != nil {
+		t.Fatalf("error reading REST session file: %s", err)
+	}
+
+	return string(vimData), string(restData)
+}
+
+func testAccClientCheckStatNoExist(t *testing.T, p string) {
+	_, err := os.Stat(p)
+	switch {
+	case err == nil:
+		t.Fatalf("expected session file %q to not exist", p)
+	case err != nil && os.IsNotExist(err):
+		return
+	case err != nil:
+		t.Fatalf("could not stat path %q: %s", p, err)
+	}
+}
+
+func TestAccClient_persistence(t *testing.T) {
+	vimSessionDir, err := ioutil.TempDir("", "tf-vsphere-test-vimsessiondir")
+	if err != nil {
+		t.Fatalf("error creating VIM session temp directory: %s", err)
+	}
+	restSessionDir, err := ioutil.TempDir("", "tf-vsphere-test-restsessiondir")
+	if err != nil {
+		t.Fatalf("error creating REST session temp directory: %s", err)
+	}
+	defer func() {
+		if err = os.RemoveAll(vimSessionDir); err != nil {
+			log.Printf("[DEBUG] Error removing test VIM session directory %q: %s", vimSessionDir, err)
+		}
+		if err = os.RemoveAll(restSessionDir); err != nil {
+			log.Printf("[DEBUG] Error removing test REST session directory %q: %s", restSessionDir, err)
+		}
+	}()
+
+	c := testAccClientGenerateConfig(t)
+	c.Persist = true
+	c.VimSessionPath = vimSessionDir
+	c.RestSessionPath = restSessionDir
+
+	expectedVim, expectedRest := testAccClientGenerateData(t, c)
+
+	// This will create a brand new session under normal circumstances
+	actualVim, actualRest := testAccClientGenerateData(t, c)
+
+	if expectedVim != actualVim {
+		t.Fatalf("VIM session data mismatch.\n\n\n\nExpected:\n\n %s\n\nActual:\n\n%s\n\n", expectedVim, actualVim)
+	}
+	if expectedRest != actualRest {
+		t.Fatalf("REST session data mismatch.\n\n\n\nExpected:\n\n %s\n\nActual:\n\n%s\n\n", expectedRest, actualRest)
+	}
+}
+
+func TestAccClient_noPersistence(t *testing.T) {
+	vimSessionDir, err := ioutil.TempDir("", "tf-vsphere-test-vimsessiondir")
+	if err != nil {
+		t.Fatalf("error creating VIM session temp directory: %s", err)
+	}
+	restSessionDir, err := ioutil.TempDir("", "tf-vsphere-test-restsessiondir")
+	if err != nil {
+		t.Fatalf("error creating REST session temp directory: %s", err)
+	}
+	defer func() {
+		if err = os.RemoveAll(vimSessionDir); err != nil {
+			log.Printf("[DEBUG] Error removing test VIM session directory %q: %s", vimSessionDir, err)
+		}
+		if err = os.RemoveAll(restSessionDir); err != nil {
+			log.Printf("[DEBUG] Error removing test REST session directory %q: %s", restSessionDir, err)
+		}
+	}()
+
+	c := testAccClientGenerateConfig(t)
+	// Just to be explicit on intent
+	c.Persist = false
+	c.VimSessionPath = vimSessionDir
+	c.RestSessionPath = restSessionDir
+
+	_, err = c.Client()
+	if err != nil {
+		t.Fatalf("error setting up client: %s", err)
+	}
+
+	vimSessionFile, err := c.vimSessionFile()
+	if err != nil {
+		t.Fatalf("error computing VIM session file: %s", err)
+	}
+	restSessionFile, err := c.restSessionFile()
+	if err != nil {
+		t.Fatalf("error computing REST session file: %s", err)
+	}
+
+	testAccClientCheckStatNoExist(t, vimSessionFile)
+	testAccClientCheckStatNoExist(t, restSessionFile)
+}
