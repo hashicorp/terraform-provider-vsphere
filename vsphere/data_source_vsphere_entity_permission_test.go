6841d9e8d753979004710a7385dde62682abc2ea
Julio Tain Sueiras <juliosueiras@gmail.com>
2018-11-30 17:18:57 -0500

    Added test cases for data source

---
+++ vsphere/data_source_vsphere_entity_permission_test.go
@@ -0,0 +1,56 @@
+package vsphere
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+)
+
+func TestAccDataSourceVSphereEntityPermission_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceVSphereEntityPermissionConfig(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.vsphere_entity_permission.terraform-test-entity-permission-data",
+						"principal",
+						testAccDataSourceVSphereEntityPermissionCheckUser,
+					),
+					resource.TestCheckResourceAttrPair(
+						"data.vsphere_entity_permission.terraform-test-entity-permission-data", "principal",
+						"vsphere_entity_permission.terraform-test-entity-permission", "principal",
+					),
+				),
+			},
+		},
+	})
+}
+
+const testAccDataSourceVSphereEntityPermissionUser = `VSPHERE.LOCAL\\Administrator`
+const testAccDataSourceVSphereEntityPermissionCheckUser = "VSPHERE.LOCAL\\Administrator"
+
+func testAccDataSourceVSphereEntityPermissionConfig() string {
+	return fmt.Sprintf(`
+variable "vsphere_user" {
+  default = "%s"
+}
+
+resource "vsphere_entity_permission" "terraform-test-entity-permission" {
+  principal   = "${var.vsphere_user}"
+	role_id     = -1
+	folder_path = "/"
+}
+
+data "vsphere_entity_permission" "terraform-test-entity-permission-data" {
+	principal   = "${var.vsphere_user}"
+}
+`,
+		testAccDataSourceVSphereEntityPermissionUser,
+	)
+}
