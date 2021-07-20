c84da4b18301b703517fea68c58fcf25a67a850a
Andy Baran <andy.baran@hashicorp.com>
2021-07-20 14:45:39 -0400

    added note about client connections

--- scripts/testing/run_tests.sh
+++ scripts/testing/run_tests.sh
@@ -1,2 +1,4 @@
+# You will likely need to 'export VSPHERE_PERSIST_SESSION=true' if you are running all tests as the API will start blocking new connection requests
+
 cd ../../
 for x in $(cat ./tests.txt); do TESTARGS="-count 1 -run $x" make testacc 2>&1 | tee ./scripts/testing/test_results/$x;done 
