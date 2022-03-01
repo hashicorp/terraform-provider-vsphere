c24e1bd56768eed3a56f6833922725949874d762
Ryan Johnson <johnsonryan@vmware.com>
2022-02-28 19:10:14 -0500

    Update`resourceVSphereDatacenterImport` to import folder
    Updates `resourceVSphereDatacenterImport` to include the datacenter folder in  which the datacenter object may exist. If found in a datacenter folder, the leading "/" is removed and the inventory path is used for the `folder` attribute during the import. If the datacenter does not reside in a folder, the value is left empty during the import. #1374
    
    Signed-off-by: Ryan Johnson <johnsonryan@vmware.com>

--- vsphere/resource_vsphere_datacenter.go
+++ vsphere/resource_vsphere_datacenter.go
@@ -13,6 +13,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/customattribute"
 	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/datacenter"
+	"github.com/hashicorp/terraform-provider-vsphere/vsphere/internal/helper/folder"
 	"github.com/vmware/govmomi/find"
 	"github.com/vmware/govmomi/object"
 	"github.com/vmware/govmomi/vim25/methods"
@@ -285,6 +286,18 @@ func resourceVSphereDatacenterImport(d *schema.ResourceData, meta interface{}) (
 	if err != nil {
 		return nil, err
 	}
+
+	f, err := folder.ParentFromPath(client, p, folder.VSphereFolderTypeDatacenter, dc)
+	if err != nil {
+		return nil, fmt.Errorf("cannot locate folder: %s", err)
+	}
+
+	path := strings.TrimPrefix(f.InventoryPath, "/")
+
+	if err := d.Set("folder", path); err != nil {
+		return nil, err
+	}
+
 	d.SetId(dc.Name())
 	return []*schema.ResourceData{d}, nil
 }
