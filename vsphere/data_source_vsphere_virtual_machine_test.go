6402c6867cd26b87789859709a120f77e4ca3ac3
Alex Pilon <apilon@hashicorp.com>
2023-04-13 19:41:33 -0400

    Set moid and add test

--- vsphere/data_source_vsphere_virtual_machine_test.go
+++ vsphere/data_source_vsphere_virtual_machine_test.go
@@ -150,6 +150,52 @@ func TestAccDataSourceVSphereVirtualMachine_uuid(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceVSphereVirtualMachine_moid(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			RunSweepers()
+			testAccPreCheck(t)
+			testAccDataSourceVSphereVirtualMachinePreCheck(t)
+		},
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceVSphereVirtualMachineConfigMOID(),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(
+						"data.vsphere_virtual_machine.moid",
+						"id",
+						regexp.MustCompile("^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$")),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "guest_id"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "scsi_type"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "memory"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "num_cpus"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "num_cores_per_socket"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "firmware"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "hardware_version"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "disks.#"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "disks.0.size"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "disks.0.eagerly_scrub"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "disks.0.thin_provisioned"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "disks.0.unit_number"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "disks.0.label"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interface_types.#"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.#"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.adapter_type"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.bandwidth_limit"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.bandwidth_reservation"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.bandwidth_share_level"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.bandwidth_share_count"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.mac_address"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "network_interfaces.0.network_id"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "firmware"),
+					resource.TestCheckResourceAttrSet("data.vsphere_virtual_machine.moid", "moid"),
+				),
+			},
+		},
+	})
+}
+
 func testAccDataSourceVSphereVirtualMachinePreCheck(t *testing.T) {
 	if os.Getenv("TF_VAR_VSPHERE_DATACENTER") == "" {
 		t.Skip("set TF_VAR_VSPHERE_DATACENTER to run vsphere_virtual_machine data source acceptance tests")
@@ -174,6 +220,28 @@ data "vsphere_virtual_machine" "template" {
 
 data "vsphere_virtual_machine" "uuid" {
   uuid = data.vsphere_virtual_machine.template.uuid
+}
+`,
+		testhelper.CombineConfigs(testhelper.ConfigDataRootDC1(), testhelper.ConfigDataRootPortGroup1()),
+		os.Getenv("TF_VAR_VSPHERE_TEMPLATE"),
+	)
+}
+
+func testAccDataSourceVSphereVirtualMachineConfigMOID() string {
+	return fmt.Sprintf(`
+%s
+
+variable "template" {
+  default = "%s"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.template
+  datacenter_id = data.vsphere_datacenter.rootdc1.id
+}
+
+data "vsphere_virtual_machine" "moid" {
+  moid = data.vsphere_virtual_machine.template.moid
 }
 `,
 		testhelper.CombineConfigs(testhelper.ConfigDataRootDC1(), testhelper.ConfigDataRootPortGroup1()),
