29c6666b7a7055ed51bea8264179c65f4ac06e48
zxinyu08 <zxinyu@vmware.com>
2023-07-19 14:10:57 +0800

    Fix #1933: Set vSAN client with the current version of the service endpoint

--- vsphere/config.go
+++ vsphere/config.go
@@ -205,10 +205,12 @@ func (c *Config) Client() (*Client, error) {
 	}
 
 	if isEligibleVSANEndpoint(client.vimClient) {
+		client.vimClient.UseServiceVersion("vsan")
 		vc, err := vsan.NewClient(ctx, client.vimClient.Client)
 		if err != nil {
 			return nil, err
 		}
+		vc.Version = client.vimClient.Version
 		client.vsanClient = vc
 	} else {
 		log.Printf("[DEBUG] Connected endpoint does not support vSAN service")
