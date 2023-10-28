8ea5e013737a3ae54c470bea103469fa27bae8d5
mristok <mristok@gmail.com>
2023-10-27 22:12:03 -0500

    feat: extend `d/host_pci_device` support
    Add support for the `vsphere_host_pci_device` data source to return all PCI devices which share the same details.

--- vsphere/data_source_vsphere_host_pci_device.go
+++ vsphere/data_source_vsphere_host_pci_device.go
@@ -5,6 +5,8 @@
 package vsphere
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"log"
 	"regexp"
 	"strconv"
@@ -39,10 +41,74 @@ func dataSourceVSphereHostPciDevice() *schema.Resource {
 				Optional:    true,
 				Description: "The hexadecimal value of the PCI device's vendor ID.",
 			},
-			"name": {
-				Type:        schema.TypeString,
+			"pci_devices": {
+				Type:        schema.TypeList,
 				Computed:    true,
-				Description: "The name of the PCI device.",
+				Description: "The list of matching PCI Devices available on the host.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The name ID of this PCI, composed of 'bus:slot.function'",
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The name of the PCI device.",
+						},
+						"bus": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The bus ID of the PCI device.",
+						},
+						"slot": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The slot ID of the PCI device.",
+						},
+						"function": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The function ID of the PCI device.",
+						},
+						"class_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The hexadecimal value of the PCI device's class ID.",
+						},
+						"vendor_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The hexadecimal value of the PCI device's vendor ID.",
+						},
+						"sub_vendor_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The hexadecimal value of the PCI device's sub vendor ID.",
+						},
+						"vendor_name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The vendor name of the PCI device.",
+						},
+						"device_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The hexadecimal value of the PCI device's device ID.",
+						},
+						"sub_device_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The hexadecimal value of the PCI device's sub device ID.",
+						},
+						"parent_bridge": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The parent bridge of the PCI device.",
+						},
+					},
+				},
 			},
 		},
 	}
@@ -50,20 +116,39 @@ func dataSourceVSphereHostPciDevice() *schema.Resource {
 
 func dataSourceVSphereHostPciDeviceRead(d *schema.ResourceData, meta interface{}) error {
 	log.Printf("[DEBUG] DataHostPCIDev: Beginning PCI device lookup on %s", d.Get("host_id").(string))
+
 	client := meta.(*Client).vimClient
+
 	host, err := hostsystem.FromID(client, d.Get("host_id").(string))
 	if err != nil {
 		return err
 	}
+
 	hprops, err := hostsystem.Properties(host)
 	if err != nil {
 		return err
 	}
+
+	// Create unique ID based on the host_id
+	idsum := sha256.New()
+	if _, err := fmt.Fprintf(idsum, "%#v", d.Get("host_id").(string)); err != nil {
+		return err
+	}
+
+	d.SetId(fmt.Sprintf("%x", idsum.Sum(nil)))
+
+	// Identify PCI devices matching name_regex (if any)
 	devices, err := matchName(d, hprops.Hardware.PciDevice)
 	if err != nil {
 		return err
 	}
+
+	// Output slice
+	pciDevices := make([]interface{}, 0, len(devices))
+
 	log.Printf("[DEBUG] DataHostPCIDev: Looking for a device with matching class_id and vendor_id")
+
+	// Loop through devices
 	for _, device := range devices {
 		// Match the class_id if it is set.
 		if class, exists := d.GetOk("class_id"); exists {
@@ -75,6 +160,7 @@ func dataSourceVSphereHostPciDeviceRead(d *schema.ResourceData, meta interface{}
 				continue
 			}
 		}
+
 		// Now match the vendor_id if it is set.
 		if vendor, exists := d.GetOk("vendor_id"); exists {
 			vendorInt, err := strconv.ParseInt(vendor.(string), 16, 16)
@@ -85,15 +171,43 @@ func dataSourceVSphereHostPciDeviceRead(d *schema.ResourceData, meta interface{}
 				continue
 			}
 		}
+
+		// Convertions
 		classHex := strconv.FormatInt(int64(device.ClassId), 16)
 		vendorHex := strconv.FormatInt(int64(device.VendorId), 16)
-		d.SetId(device.Id)
-		_ = d.Set("name", device.DeviceName)
-		_ = d.Set("class_id", classHex)
-		_ = d.Set("vendor_id", vendorHex)
+		subVendorHex := strconv.FormatInt(int64(device.SubVendorId), 16)
+		deviceHex := strconv.FormatInt(int64(device.DeviceId), 16)
+		subDeviceHex := strconv.FormatInt(int64(device.SubDeviceId), 16)
+		busString := fmt.Sprintf("%v", device.Bus)
+		slotString := fmt.Sprintf("%v", device.Slot)
+		functionString := fmt.Sprintf("%v", device.Function)
+
+		dev := map[string]interface{}{
+			"id":            device.Id,
+			"name":          device.DeviceName,
+			"class_id":      classHex,
+			"vendor_id":     vendorHex,
+			"sub_vendor_id": subVendorHex,
+			"device_id":     deviceHex,
+			"sub_device_id": subDeviceHex,
+			"bus":           busString,
+			"slot":          slotString,
+			"function":      functionString,
+			"parent_bridge": device.ParentBridge,
+			"vendor_name":   device.VendorName,
+		}
+
+		// Add PCI device to output slice
+		pciDevices = append(pciDevices, dev)
+
 		log.Printf("[DEBUG] DataHostPCIDev: Matching PCI device found: %s", device.DeviceName)
-		return nil
 	}
+
+	// Set the `pci_devices` output to all PCI devices
+	if err := d.Set("pci_devices", pciDevices); err != nil {
+		return err
+	}
+
 	return nil
 }
 
