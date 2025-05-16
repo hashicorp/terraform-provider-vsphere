c3dae51ae7165d7b28de5154c87954f46cf7cc44
Ryan Johnson <ryan.johnson@broadcom.com>
2025-05-16 10:47:36 -0400

    fix: address G107 gosec warnings in HTTP requests (#2510)
    Replace direct `http.Get()` calls with `http.Client` and explicit `http.NewRequest` to address G107 warnings (Potential HTTP request made with variable URL).
    
    Signed-off-by: Ryan Johnson <ryan.johnson@broadcom.com>

--- vsphere/resource_vsphere_content_library_item_test.go
+++ vsphere/resource_vsphere_content_library_item_test.go
@@ -144,7 +144,12 @@ func testAccResourceVSphereContentLibraryItemGetOva() {
 }
 
 func testAccResourceVSphereContentLibraryItemGetFile(url, file string) error {
-	resp, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return err
+	}
+	resp, err := client.Do(req)
 	if err != nil {
 		return err
 	}
