b6b49d18d0027d8f28fed7080e5ff99980f5dac6
kiliev <kiliev@vmware.com>
2025-06-04 15:49:00 +0300

    install gotestfmt

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-. setup_env_vars.sh
+. setup_env_vars.sha
+go install github.com/gotesttools/gotestfmt/v2/cmd/gotestfmt@latest
 TF_ACC=1 go test -json -v ../vsphere -timeout 360m 2>&1 | tee gotest.log | gotestfmt
