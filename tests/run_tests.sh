8fe28e0914caf5cfacaf955b61b7442719bfa89a
kiliev <kiliev@vmware.com>
2025-06-10 15:24:45 +0300

    fix small syntax error

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-. setup_env_vars.sha
+. setup_env_vars.sh
 go install github.com/gotesttools/gotestfmt/v2/cmd/gotestfmt@latest
 TF_ACC=1 go test -json -v ../vsphere -timeout 360m 2>&1 | tee gotest.log | gotestfmt
